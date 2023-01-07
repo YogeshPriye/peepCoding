c2bea25e493a7946762ef4827337564c59cf92c0
YogeshPriye <yogeshpriye8797@gmail.com>
2023-01-07 19:18:29 +0530

    commit

---
+++ stacksAndQueues/SlidingWindowMaximum.java
@@ -0,0 +1,24 @@
+/**
+ * https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/sliding-window=maximum-official/ojquestion
+ */
+package stacksAndQueues;
+import java.io.*;
+import java.util.*;
+
+public class SlidingWindowMaximum{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    // code
+    
+ }
+}
