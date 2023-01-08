87ac3f4c35936e4d0861e27227ece366a5324423
YogeshPriye <yogeshpriye8797@gmail.com>
2023-01-08 17:15:14 +0530

    commit

--- stacksAndQueues/SlidingWindowMaximum.java
+++ stacksAndQueues/SlidingWindowMaximum.java
@@ -19,6 +19,30 @@ public static void main(String[] args) throws Exception {
     int k = Integer.parseInt(br.readLine());
 
     // code
-    
+    Stack<Integer> st = new Stack<>();
+    int arr[] = new int[n];
+    st.push(n - 1);
+    arr[n - 1] = n;
+    for (int i = a.length - 2; i >= 0; i--) {
+    	while (st.size() > 0 && a[i] >= a[st.peek()]) {
+    		st.pop();
+    	}
+    	if (st.size() == 0 ) {
+    		arr[i] = n;
+    	} else {
+    		arr[i] = st.peek();
+    	}
+    	st.push(i);
+    }
+    int j = 0;
+    for (int i = 0; i <= n - k; i++) {
+    	if (j < i) {
+    		j = i;
+    	}
+    	while(arr[j] < i + k) {
+    		j = arr[j];
+    	}
+    	System.out.println(a[j]);
+    }    
  }
 }
